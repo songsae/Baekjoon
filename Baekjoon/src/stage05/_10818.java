95d61705c35d20a528012167fb3310a9d4577a4a
songsae <shm4873@hanmail.net>
2020-09-18 21:51:22 +0900

    stage5 1차원 배열

---
+++ Baekjoon/src/stage05/_10818.java
@@ -0,0 +1,27 @@
+package stage05;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _10818 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		bw.write(arr[0] + " " + arr[n-1]);
+		bw.flush();
+		bw.close();
+	}
+}
